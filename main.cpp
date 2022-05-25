317b5f3cc98525c7ab806f14522670577b28a74a
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2022-05-25 09:31:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ int convertToInstruction(std::string operacao,int op1,int op2,int op3){
 
 //compile then run it with ./main < file.txt
 int main(){
+    processador PROCESSADOR;
     std::string line;
     while(std::getline (std::cin,line)){
         std::stringstream ss(line); 
@@ -43,11 +44,13 @@ int main(){
             operandos.push_back(0);
         }
         if(operation!="memset"){
-            int instrucao = convertToInstruction(operation,operandos[0],operandos[1],operandos[2]);
+            sc_uint<32> instrucao = convertToInstruction(operation,operandos[0],operandos[1],operandos[2]);
+            PROCESSADOR.MEM_INSTRUCOES.instrucoes.push_back(instrucao);
         }
         else{
-            //seta espaço de memoria operandos[0] para operandos[1]
-            std::cout<<operandos[0]<<" "<<operandos[1]<<std::endl;
+            int posicao = operandos[0];
+            sc_uint<32> valor = operandos[1];
+            PROCESSADOR.MEM_DADOS.banco[posicao]=valor;
         }
     }
 }
