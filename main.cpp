94e772e899e436afef1e831dce262fbd03338b9a
João Victor Malheiros Farias de Aquino <38818400+Malhe1ros@users.noreply.github.com>
2022-05-26 14:18:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -61,7 +61,27 @@ int sc_main(int argc, char* argv[]) {
         }
         
     }
-    std::cout<<PROCESSADOR.MEM_DADOS.banco[4]<<std::endl;
+    
+    sc_signal<bool> clock;
+    PROCESSADOR.sinal_clock(clock);
+    int numberCycles=0;
+	
+    while (not sc_end_of_simulation_invoked()) {
+		clock = 0;
+        sc_start(1, SC_NS);
 
+		clock = 1;
+		sc_start(1, SC_NS);
+		numberCycles++;
+	}
+    std::cout<<numberCycles<<std::endl;
+    for(int i=0;i<20;i++){
+        std::cout<<PROCESSADOR.BANCO_REGISTRADORES.banco[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<20;i++){
+        std::cout<<PROCESSADOR.MEM_INSTRUCOES.instrucoes[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
